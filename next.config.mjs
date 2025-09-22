62cd68cb4d8950f9f1313076e42d47ba159ec1f9
technovinky <contact@technovinky.com>
2025-09-22 09:04:54 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: 'export', // <-- enables `next export`
+}
+
+export default nextConfig
